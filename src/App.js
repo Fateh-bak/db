b288b960ee64da8cb812e4b6dfe1f566c9c6a79b
Fateh-bak <fatehbakiri6@gmail.com>
2024-08-06 03:03:51 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import { qcms } from './qcms';
 function App() {
   const [selectedOption, setSelectedOption] = useState("default");
   const [qcmslist, setqcmslist] = useState([])
-  const [year, setyear] = useState(0)
+  const [year, setyear] = useState("0")
+  const [qcmnum,setqcmnum] = useState(0)
 // Sample data
 useEffect(()=>{
   if(!selectedOption || selectedOption == "default"){
     return
   }
-  let creatingdisplayedqcmlist = qcms.filter((qcm)=>(qcm.mtitle==selectedOption && qcm.annee == 2022))
 
+  let creatingdisplayedqcmlist = qcms.filter((qcm)=>(qcm.mtitle==selectedOption))
+  if(year!=="0"){
+    creatingdisplayedqcmlist = creatingdisplayedqcmlist.filter((qcm)=>(parseInt(qcm.annee)===parseInt(year)))
+  }
+  setqcmnum(creatingdisplayedqcmlist.length)
   setqcmslist(creatingdisplayedqcmlist)
 
-},[selectedOption])
+},[selectedOption,year])
 const data = [
   {
     "mtitle": "PÉDIATRIE ",
@@ -40,7 +45,11 @@ const data = [
   }
 ];
 
-  const handleChange = (event) => {
+  const handleChange = (event,type) => {
+    if(type==="year"){
+      setyear(event.target.value)
+      return
+    }
     setSelectedOption(event.target.value);
   };
 
@@ -48,7 +57,7 @@ const data = [
   return (
     <div style={{ padding: '20px' }}>
       <h1>base des données QCM setif Prototype</h1>
-      <label htmlFor="data-select">Select Item:</label>
+      <label htmlFor="data-select">Module:</label>
       <select id="data-select" value={selectedOption} onChange={handleChange}>
       <option key="default" value="default" disabled >select</option>
       {
@@ -59,20 +68,38 @@ const data = [
           </option>
 )) : ""
       }</select>
+      
+      <label htmlFor="data-select">année:</label>
+      <select id="data-select" value={year} onChange={(e)=>{handleChange(e,"year")}}>
+      <option key="default" value="0" disabled >select</option>
+      <option key="2023" value="2023"  >2023</option>
+      <option key="2022" value="2022"  >2022</option>
+      <option key="2021" value="2021"  >2021</option>
+      <option key="2020" value="2020"  >2020</option>
+      <option key="2019" value="2019"  >2019</option>
+      <option key="2018" value="2018"  >2018</option>
+      <option key="2017" value="2017"  >2017</option>
+      <option key="2016" value="2016"  >2016</option>
+      <option key="2015" value="2015"  >2015</option>
+      <option key="2014" value="2014"  >2014</option>
+      <option key="2013" value="2013"  >2013</option>
+      <option key="2012" value="2012"  >2012</option>
+      <option key="2011" value="2011"  >2011</option>
+      <option key="2010" value="2010"  >2010</option>
 
+
+      </select>
 <div className='listq'>
+<div> 
+<h2>Nombre des qcm <span>{qcmnum}</span></h2>
+
+</div>
 {qcmslist?.length && selectedOption ?
         qcmslist.map((currentData,index)=>(<div className='ex'  style={{ marginTop: '20px' }}>
         <h2>{currentData.mtitle}</h2>
         <h3>{currentData.ctitle}</h3>
         <h4>{currentData.stitle}</h4>
         <p><strong>{currentData.qcm_question}</strong></p>
-        <ul>
-          {currentData.qcm_choices.map((choice, index) => (
-            <li key={index}>{choice}</li>
-          ))}
-        </ul>
-        <p><strong>Correct Answers:</strong></p>
         <ul>
           {currentData.qcm_choices.map((choice, index) => (
             <li key={index}>
